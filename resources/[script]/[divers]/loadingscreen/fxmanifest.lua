eb58e85a143a6b2bf92a555fdfe3930d9b0f7cfb
Freetz <110610754+Freeetz@users.noreply.github.com>
2023-09-05 20:44:54 +0200

    Add files via upload

---
+++ resources/[script]/[divers]/loadingscreen/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+game "gta5"
+
+files {
+    'index.html',
+    'style.css',
+	'index.js',
+    'logo.png',
+    'music/music.mp3',
+    'images/*.png'
+}
+
+loadscreen 'index.html'
