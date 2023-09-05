c5b2bef0ff40d0f07f1a8c300747bc089b0d3e61
Freetz <110610754+Freeetz@users.noreply.github.com>
2023-09-05 20:55:23 +0200

    Add files via upload

---
+++ resources/[script]/[divers]/Freetz-Core/scripts/[libs]/hardcap/client/main.lua
@@ -0,0 +1,13 @@
+-------- [Otopia] dev par Freetz -------
+
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+
+		if NetworkIsSessionStarted() then
+			TriggerServerEvent('hardcap:playerActivated')
+			return
+		end
+	end
+end)
